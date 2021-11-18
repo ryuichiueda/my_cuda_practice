7f37d73b2dac3bd0c60f86a42e85016fd903a5df
Ryuichi Ueda <ryuichiueda@gmail.com>
2021-11-18 13:34:41 +0900

    Make benchmark possible

--- array2d.cpp
+++ array2d.cpp
@@ -1,14 +1,6 @@
 #include <iostream>
 #include <chrono>
 
-__global__ void MatAdd(float *A, float *B, float *C)
-{
-    int block_idx = blockIdx.x*blockDim.x*blockDim.y;
-    int i = threadIdx.x + blockDim.x * threadIdx.y + block_idx;
-    for(int k=0;k<1024;k++)
-        C[i] += A[i]*3.14 + B[i]/3.14;
-}
-
 int main(int argc, char **argv)
 {
     int N = atoi(argv[1]);
@@ -25,18 +17,14 @@ int main(int argc, char **argv)
 
     auto start = std::chrono::system_clock::now();
 
-    float *a, *b, *c;
-    cudaMalloc(&a, N*N*sizeof(float));
-    cudaMalloc(&b, N*N*sizeof(float));
-    cudaMalloc(&c, N*N*sizeof(float));
+    for(int j=0;j<N;j++){
+    	for(int i=0;i<N;i++){
+           for(int k=0;k<1024;k++)
+                C[i + j*N] += A[i + j*N]*3.14 + B[i + j*N]/3.14;
+	}
+    }
+    
 
-    cudaMemcpy(a, A, N*N*sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(b, B, N*N*sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(c, C, N*N*sizeof(float), cudaMemcpyHostToDevice);
-
-    MatAdd<<<N, N>>>(a, b, c);
-
-    cudaMemcpy(C, c, N*N*sizeof(float), cudaMemcpyDeviceToHost);
     auto end = std::chrono::system_clock::now();
     auto dur = end - start;
     std::cerr << std::chrono::duration_cast<std::chrono::milliseconds>(dur).count() << "msec" << std::endl;
@@ -48,11 +36,6 @@ int main(int argc, char **argv)
 	}
 	std::cout << std::endl;
     }
-
-
-    cudaFree(a);
-    cudaFree(b);
-    cudaFree(c);
 
     return 0;
 }
